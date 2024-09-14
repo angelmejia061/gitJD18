0763ea257d1b52b07b4e3564928cf6e1cbb91ae6
Ángel Méndez <angel.mej0610@gmail.com>
2024-09-14 12:11:35 +0200

    Modifique el mensaje en clase Main

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,10 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        String miTexto = "Bienvenidos a Java Developer";
+        //Para imprimir rapido una linea de texto string entonces podemos emplear el comando "soutv"
+        System.out.println("Hola " + miTexto);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        //System.out.printf("Hello and welcome!");
     }
 }
